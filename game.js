e3d782260c844a600ffbf7896e197297a081d906
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-18 11:56:57 +0000

    Added functionality to game objects

--- game.js
+++ game.js
@@ -51,3 +51,29 @@ var reset = function () {
   monster.x = 32 + (Math.random() * (canvas.width - 64));
   monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
+
+// UPDATE GAME OBJECTS
+var update = function (modifier) {
+  if (38 in keysDown) { // Player holding up
+    hero.y -= hero.speed * modifier;
+  }
+  if (40 in keysDown) { // Player holding down
+    hero.y += hero.speed * modifier;
+  }
+  if (37 in keysDown) { // Player holding left
+    hero.x -= hero.speed * modifier; 
+  }
+  if (39 in keysDown) { // Player holding right
+    hero.x += hero.speed * modifier;
+  }
+
+  // Are the hero and monster in the same position?
+
+  if (
+    hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32)
+    && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32)
+  ) {
+    ++monstersCaught;
+    reset()
+  }
+};
