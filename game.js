370c27d5a9e16aeedd5cd6fa2b6a6ca9e14bbe5c
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-18 11:34:40 +0000

    Added game objects, hero & monster

--- game.js
+++ game.js
@@ -7,10 +7,22 @@ canvas.height = 480;
 document.body.appendChild(canvas);
 
 // BACKGROUND
-
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
   bgReady = true;
 };
 bgImage.src = "img/background.png";
+
+// GAME OBJECTS
+var hero = {
+  speed: 256, //Movement speed in pixels
+  x: 0,
+  y: 0
+  //Movement direction
+};
+var monster = {
+  x: 0,
+  y: 0
+};
+var monstersCaught = 0; //Counter
