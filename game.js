8d37032d8e57d59fc0d90cd1817958d2c5d5ed96
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-01-18 11:32:21 +0000

    Added background image

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// CANVAS
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 
@@ -5,3 +6,11 @@ canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
+// BACKGROUND
+
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+  bgReady = true;
+};
+bgImage.src = "img/background.png";
